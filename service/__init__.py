41a94329ccc58c725414327b1a6f237f05ce7687
AKSHAY-JADHAV-0 <140157214+AKSHAY-JADHAV-0@users.noreply.github.com>
2023-12-23 11:58:19 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -9,7 +9,9 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
 talisman = Talisman(app)
+CORS(app)
 
 # Create Flask application
 app = Flask(__name__)
